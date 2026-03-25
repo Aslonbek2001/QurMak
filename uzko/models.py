8ed656e7b26c26acad24f096436773f4f21a9058
Aslon <arslon@gmail.com>
2026-03-25 16:41:44 +0500

    Commit

--- uzko/models.py
+++ uzko/models.py
@@ -2,9 +2,9 @@ from django.db import models
 # from UzTransliterator import UzTransliterator
 
 class VocabModel(models.Model):
-    korean = models.CharField(max_length=100, db_index=True)
+    korean = models.TextField(db_index=True)
     uzb = models.CharField(max_length=100, db_index=True)
-    krill = models.CharField(max_length=100,db_index=True, null=True, blank=True)
+    krill = models.CharField(max_length=100, db_index=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
 
