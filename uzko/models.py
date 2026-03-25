d0cfc4cf15e892f9138c279b350390d8faa0714e
Aslon <arslon@gmail.com>
2026-03-25 16:47:30 +0500

    Commit

--- uzko/models.py
+++ uzko/models.py
@@ -2,11 +2,16 @@ from django.db import models
 # from UzTransliterator import UzTransliterator
 
 class VocabModel(models.Model):
-    korean = models.TextField(db_index=True)
-    uzb = models.TextField(db_index=True)
-    krill = models.TextField(db_index=True)
+    korean = models.TextField(db_index=True, verbose_name="Qurilish atama nomi")
+    uzb = models.TextField(db_index=True, verbose_name="O'zbek tilida")
+    krill = models.TextField(db_index=True, verbose_name="Krill tilida", null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Qurilish atamasi "
+        verbose_name_plural = "Qurilish atamalar "
+
 
 
     # def save(self, *args, **kwargs):
