acc1130abbd266fe0cb5a238e8fa19ec16741e60
Aslon <arslon@gmail.com>
2026-03-25 16:43:27 +0500

    Commit

--- uzko/models.py
+++ uzko/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class VocabModel(models.Model):
     korean = models.TextField(db_index=True)
-    uzb = models.CharField(max_length=100, db_index=True)
-    krill = models.CharField(max_length=100, db_index=True)
+    uzb = models.TextField(db_index=True)
+    krill = models.TextField(db_index=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
 
